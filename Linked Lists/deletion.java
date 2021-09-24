7d02ccf338de6a1aa2da79030d121edd3fcbdd55
HarshVikram <harsh.vikram34@gmail.com>
2021-09-24 06:35:43 +0530

    added insertion, deletion and searching in linked list

--- Linked Lists/deletion.java
+++ Linked Lists/deletion.java
@@ -24,6 +24,14 @@ public class LinkedList {
 			last.next = new_node;
 		}
 	}
+	public static void deletion(int position) {
+		Node temp = head;
+
+		for (int i = 0; temp != nul; && i < position - 1; i++)
+			temp = temp.next;
+
+		temp.next = temp.next.next;  
+	}
 	public static void print(LinkedList list) {
 		Node curr_node = list.head;
 		System.out.print("Linked List: ");
@@ -35,14 +43,19 @@ public class LinkedList {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		LinkedList list = new LinkedList();
-		list = insert(list, 1);
-        list = insert(list, 2);
-        list = insert(list, 3);
-        list = insert(list, 4);
-        list = insert(list, 5);
-        list = insert(list, 6);
-        list = insert(list, 7);
-        list = insert(list, 8);
+		list.insert(list, 1);
+        list.insert(list, 2);
+        list.insert(list, 3);
+        list.insert(list, 4);
+        list.insert(list, 5);
+        list.insert(list, 6);
+        list.insert(list, 7);
+        list.insert(list, 8);
+
+        print(list);
+
+        list.deletion(5);
+        list.deletion(2);
 
         print(list);
     }
--- Linked Lists/searching.java
+++ Linked Lists/searching.java
@@ -24,6 +24,15 @@ public class LinkedList {
 			last.next = new_node;
 		}
 	}
+	public static void replace(int position, int data) {
+		Node curr_node = head;
+		Node new_node = new Node(data);
+		for (int i = 0; temp != nul; && i < position - 1; i++)
+			curr_node = curr_node.next;
+
+		new_node.next = curr_node.next.next;
+		curr_node.next = new_node; 
+	}
 	public static void print(LinkedList list) {
 		Node curr_node = list.head;
 		System.out.print("Linked List: ");
@@ -35,14 +44,19 @@ public class LinkedList {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		LinkedList list = new LinkedList();
-		list = insert(list, 1);
-        list = insert(list, 2);
-        list = insert(list, 3);
-        list = insert(list, 4);
-        list = insert(list, 5);
-        list = insert(list, 6);
-        list = insert(list, 7);
-        list = insert(list, 8);
+		list.insert(list, 1);
+        list.insert(list, 2);
+        list.insert(list, 3);
+        list.insert(list, 4);
+        list.insert(list, 5);
+        list.insert(list, 6);
+        list.insert(list, 7);
+        list.insert(list, 8);
+
+        print(list);
+
+        list.replace(0);
+        list.replace(0);
 
         print(list);
     }
